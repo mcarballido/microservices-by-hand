c915bc72e4f572c9171cc8782019835706a675f8
Manuel Carballido <manu.carba96@gmail.com>
2020-07-09 13:21:56 -0300

    Add moderation service

--- frontend/src/CommentList.js
+++ frontend/src/CommentList.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
 
 export default ({ comments }) => {
+  const renderedComments = comments.map((comment) => {
+    let content;
 
-  const renderedComments = comments.map(comment => {
-    return <li key={comment.id}>{comment.content}</li>;
+    switch (comment.status) {
+      case "pending":
+        content = "This comment is awaiting moderation.";
+        break;
+      case "approved":
+        content = comment.content;
+        break;
+      case "rejected":
+        content = "This comment has been rejected.";
+        break;
+      default:
+        break;
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
